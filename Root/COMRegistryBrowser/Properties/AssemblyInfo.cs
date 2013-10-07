fcc37dcb56af169869ada50d2186a398d9fbcf62
tomenglert_cp <SND\tomenglert_cp>
2013-10-07 08:54:56 +0000

    Show ProgID of servers.

--- Root/COMRegistryBrowser/Properties/AssemblyInfo.cs
+++ Root/COMRegistryBrowser/Properties/AssemblyInfo.cs
@@ -1,17 +1,14 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 [assembly: AssemblyTitle("COM Registry Browser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("tom-englert.de")]
 [assembly: AssemblyProduct("Tom's Toolbox")]
-[assembly: AssemblyCopyright("Copyright © 2012 tom-englert.de")]
+[assembly: AssemblyCopyright("Copyright © 2013 tom-englert.de")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
